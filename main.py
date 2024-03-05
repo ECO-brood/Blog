54eb17455979465544eee9748b68e77499013e96
ECO-brood <135516651+ECO-brood@users.noreply.github.com>
2024-03-05 08:31:25 +0000

    registration page

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+import
+from flask import Flask, render_template , request , redirect
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return render_template('index.html')
+@app.route('/home', methods=['POST' , 'GET'])
+def home():
+    Full_name = request.form['Full_name']
+    Email = request.form['Email']
+    password = request.form['password']
+    return render_template('home.html' , Full_name=Full_name , Email=Email , password=password)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
